7749c7a4c672072f7d06c65b75a5a352c18d748d
Gary Cordero <GaryCordero1690@gmail.com>
2021-04-02 22:52:07 -0400

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "./Components/Layout/Layout"
+import CustomerView from './Components/Customer/CustomerView';
 
 function App() {
   return (
     <div>
       <Layout>
         <div>
-          <p>Main page</p>
+        {/* <h2>Collapsible Sidebar Using Bootstrap 4</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
+        <CustomerView/>
         </div>
       </Layout>
     </div>
